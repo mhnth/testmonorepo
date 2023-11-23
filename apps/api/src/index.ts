d63c5c0f9feef65a1ccd3c5e80f34effdf5b027f
manhnth <n.t.manh@viscirclevn.com>
2023-11-23 15:38:37 +0700

    public

---
+++ apps/api/src/index.ts
@@ -0,0 +1,29 @@
+import { json, urlencoded } from 'body-parser';
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
+export const createServer = () => {
+  const app = express();
+  app
+    .disable('x-powered-by')
+    .use(morgan('dev'))
+    .use(urlencoded({ extended: true }))
+    .use(json())
+    .use(cors())
+    .get('/hello/:name', (req, res) => {
+      return res.json({ message: `hello ${req.params.name}` });
+    })
+    .get('/healthz', (req, res) => {
+      return res.json({ ok: true });
+    });
+
+  return app;
+};
+
+const port = process.env.PORT || 5000;
+const server = createServer();
+
+server.listen(port, () => {
+  console.log(`api running on ${port}`);
+});
